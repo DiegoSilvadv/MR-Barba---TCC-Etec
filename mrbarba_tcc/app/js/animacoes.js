d7767f64392e0e5a338f8914c2247b5c5295297b
DiegoSilvadv <digssilva2000@outlook.com>
2020-07-22 17:51:51 -0300

    Ajustes no layout
    Foi adicionado, animações básicas ao projeto

---
+++ mrbarba_tcc/app/js/animacoes.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#background-yellow").hide();
+    $("#background-yellow").slideDown(1500);
+   
+    $(".descricao-d").hide();
+    $(".descricao-d").slideDown(1500);
+    $(".descricao").hide();
+    $(".descricao").slideDown(1500);
+    
+});
